f9c164fae032d50cbe58fa4fcd8d3aced88b1b9a
Naman Vyas <dellLaptop@naman.com>
2022-05-01 02:36:37 +0530

    Initial commit

---
+++ scripts/deploy.py
@@ -0,0 +1,21 @@
+from brownie import SimpleStorage, accounts, network, config
+
+def deploy_simple_storage():
+    account = get_account()
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    print(transaction.block_number)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def main():
+    deploy_simple_storage()
